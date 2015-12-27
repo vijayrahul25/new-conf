c20ef66e591acc2a6f0c92963f3b3bf6dcc9834b
vijayrahul25 <vijayrahul25@gmail.com>
2015-12-27 18:34:43 +0530

    second
    v

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Rahul first succesfull application' });
 });
 
 module.exports = router;
